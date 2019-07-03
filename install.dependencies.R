fc2db7bffd21316e8175f0ac2e62345691b7047c
lthealy <lthealy44@gmail.com>
2019-07-03 18:47:51 +0000

    Added install.dependencies.R
    Updated description to Encoding: UTF-8 to allow for Roxygen2
    
    Added GEO_query walkthrough
    
    Loaded .Rd from Ginnie's files b/c roxygen

---
+++ install.dependencies.R
@@ -0,0 +1,22 @@
+if (!require("Metrics")) {
+   install.packages("Metrics", dependencies = TRUE, repos="http://cran.rstudio.com/")
+}
+if (!require("NMF")) {
+   install.packages("NMF", dependencies = TRUE, repos="http://cran.rstudio.com/")
+}
+if (!require("gtools")) {
+   install.packages("gtools", dependencies = TRUE, repos="http://cran.rstudio.com/")
+}
+if (!require("mvnfast")) {
+   install.packages("mvnfast", dependencies = TRUE, repos="http://cran.rstudio.com/")
+}
+
+# ===============================
+#          Bioconductor
+# ===============================
+if (!require("BiocManager")) {
+   install.packages("BiocManager")
+}
+if (!require("GEOquery")) {
+   BiocManager::install("GEOquery")
+}
