b8d903b341a65c9a1c6c313c9ef145e8ec0b3e5d
anuvabanwasi <anuva.banwasi@gmail.com>
2019-07-08 19:06:32 -0400

    parse GSE data

--- R/function.R
+++ R/function.R
@@ -1,10 +1,44 @@
 library(GEOquery)
 parse <- function() {
-  gse <- getGEO("GSE30611", GSEMatrix = TRUE)
-  gse <- gse$GSE30611_series_matrix.txt.gz
-  parsedGSE30611 <- list()
-  parsedGSE30611$expression <- exprs(gse)
-  parsedGSE30611$sampleInfo <- gse@phenoData
-  parsedGSE30611$featureInfo <- data.frame(SYMBOL = rownames(expression))
-  save(parsedGSE30611, file = "./data/datasetGSE30611.RData")
+  #-----------------------------GSE19830-----------------------------------
+  
+  gse <- getGEO("GSE19830", GSEMatrix = TRUE, getGPL = FALSE)
+  gse <- gse$GSE19830_series_matrix.txt.gz
+  expression <- exprs(gse)
+  sampleInfo <- gse@phenoData
+  featureInfo <- data.frame(SYMBOL = rownames(expression))
+  parsedGSE19830 <- list()
+  parsedGSE19830$expression <- exprs(gse)
+  parsedGSE19830$sampleInfo <- sampleInfo@data
+  parsedGSE19830$featureInfo <- data.frame(SYMBOL = rownames(expression))
+  
+  #-----------------------------GSE97284-----------------------------------
+  
+  gse <- getGEO("GSE97284", GSEMatrix = TRUE, getGPL = FALSE)
+  gse <- gse$GSE97284_series_matrix.txt.gz
+  expression <- exprs(gse)
+  sampleInfo <- gse@phenoData
+  featureInfo <- data.frame(SYMBOL = rownames(expression))
+  parsedGSE97284 <- list()
+  parsedGSE97284$expression <- exprs(gse)
+  parsedGSE97284$sampleInfo <- sampleInfo@data
+  parsedGSE97284$featureInfo <- data.frame(SYMBOL = rownames(expression))
+  
+  #-----------------------------GSE127472-----------------------------------
+  
+  gse <- getGEO("GSE127472", GSEMatrix = TRUE, getGPL = FALSE)
+  gse <- gse$`GSE127472-GPL570_series_matrix.txt.gz`
+  expression <- exprs(gse)
+  sampleInfo <- gse@phenoData
+  featureInfo <- data.frame(SYMBOL = rownames(expression))
+  parsedGSE127472 <- list()
+  parsedGSE127472$expression <- expression
+  parsedGSE127472$sampleInfo <- sampleInfo@data
+  parsedGSE127472$featureInfo <- featureInfo
+  
+  #-----------------------------save-----------------------------------
+  
+  save(parsedGSE19830, file = "./data/datasetGSE19830.RData")
+  save(parsedGSE97284, file = "./data/datasetGSE97284.RData")
+  save(parsedGSE127472, file = "./data/datasetGSE127472.RData")
 }
