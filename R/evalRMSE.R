045af026d7fd648a2ebbbd777d04424147510b30
ginniema <40671794+ginniema@users.noreply.github.com>
2019-07-09 15:15:41 -0400

    Minor update: flexible # cell types

--- R/evalRMSE.R
+++ R/evalRMSE.R
@@ -5,9 +5,6 @@
 
 evalRMSE = function(truth, observed)
 {
-  RMSE = c(rmse(truth[,1], observed[,1]),
-           rmse(truth[,2], observed[,2]),
-           rmse(truth[,3], observed[,3]),
-           rmse(truth[,4], observed[,4]))
+  RMSE = sapply(1:ncol(truth), function(i) rmse(truth[,i], observed[,i]))
   return(RMSE)
 }
