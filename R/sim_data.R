86a746f9de40079a06bdf7f46cb9a6b21ec97197
ginniema <40671794+ginniema@users.noreply.github.com>
2019-07-09 16:07:49 -0400

    Update sim_data.R

--- R/sim_data.R
+++ R/sim_data.R
@@ -7,40 +7,14 @@
 sim_data = function(mu_tumor = 101-(1:100),
                     A_tumor = 1:100 %*% t(1:100),
                     H = sim_H(),
-                    scaleFactor = 10,
+                    scaleFactor = rep(10,3),
                     seed = 1234)
 {
-  stroma_info = sortMu(mu = mu_tumor,
-                       A = A_tumor,
-                       scaleFactor = scaleFactor,
-                       seed = seed+1)
-  
-  immune_info = sortMu(mu = mu_tumor,
-                       A = A_tumor,
-                       scaleFactor = scaleFactor,
-                       seed = seed+2)
-  
-  normal_info = sortMu(mu = mu_tumor,
-                       A = A_tumor,
-                       scaleFactor = scaleFactor,
-                       seed = seed+3)
-  
-  mu_stroma = stroma_info[[1]]
-  A_stroma = stroma_info[[2]]
-  mu_immune = immune_info[[1]]
-  A_immune = immune_info[[2]]
-  mu_normal = normal_info[[1]]
-  A_normal = normal_info[[2]]
-  
-  mu = cbind(mu_tumor,
-             mu_stroma,
-             mu_immune,
-             mu_normal)
-  
-  A = list(A_tumor,
-           A_stroma,
-           A_immune,
-           A_normal)
+  if (length(scaleFactor)!=(nrow(H)-1))
+  {
+    stop("Error: parameters don't match in length")
+  }
+  numTypes = nrow(H)
   
   # for each sample's mixture matrix
   # ... and a set of mvn params
