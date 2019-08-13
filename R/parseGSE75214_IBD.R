d8d84558c8cc1a0cc1d31df8d42b49ab0a4f87d6
anuvabanwasi <anuva.banwasi@gmail.com>
2019-08-13 05:19:59 -0700

    Parsed data for GSE75214 (for Dr.Montrose)

---
+++ R/parseGSE75214_IBD.R
@@ -0,0 +1,33 @@
+#' generate_data_set_list
+#'
+#' @export
+#' @import GEOquery
+
+
+#-----------------------------GSE75214-----------------------------------
+
+parseGSE75214 <- function() {
+  # library(GEOquery)
+  
+  gse <- getGEO("GSE75214", 
+                GSEMatrix = TRUE, 
+                getGPL = FALSE)
+  gse <- gse$GSE75214_series_matrix.txt.gz
+  
+  ex <- exprs(gse)
+  sampInfo <- gse@phenoData
+  featInfo <- data.frame(SYMBOL = rownames(ex))
+  parsedGSE19830 <- list()
+  parsedGSE19830$ex <- ex
+  parsedGSE19830$sampInfo <- sampInfo@data
+  parsedGSE19830$featInfo <- featInfo
+  parsedGSE19830$metadata <- list(log.transformed = FALSE,
+                                  reference = "Vancamelbeke M, Vanuytsel T, Farré R, Verstockt S et al. Genetic and Transcriptomic Bases of Intestinal Epithelial Barrier Dysfunction in Inflammatory Bowel Disease. Inflamm Bowel Dis 2017 Oct;23(10):1718-1729. PMID: 28885228",
+                                  accession = "GSE75214",
+                                  description = "Mucosal biopsies from colon of 97 UC patients, 8 CD patients, 11 controls and mucosal biopsies from illeum of 67 CD patients, 11 controls",
+                                  survivalA = "None",
+                                  survivalB = "None")
+  save(parsedGSE19830, file = "./data/datasetGSE75214.RData")
+  return(NULL)
+}
+
