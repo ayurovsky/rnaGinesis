0502af26ac5c17362b74455a70ebd6bdcd32e32d
lthealy <luke.torrehealy@stonybrookmedicine.edu>
2019-11-12 16:30:40 -0500

    Corrected function to break down by gene

--- R/plot_GEPxFRAC.R
+++ R/plot_GEPxFRAC.R
@@ -5,26 +5,28 @@
 #' @import ggplot2
 #' @import reshape2
 
-plot_GEPxFrac <- function(data = parsed_projects){
+plot_GEPxFrac <- function(data = parsed_projects, gene = c("PHGDH", "IL27")){
   BigData <- NULL
   for (i in names(data)){
     this_proj <- data[[i]]
 
-    melted.gep <- reshape2::melt(this_proj[[7]], value.name = "GEP")
-    melted.gep <- aggregate(melted.gep[,3],
-                            list(melted.gep$variable),
-                            sum)
+    index <- which(this_proj[[7]]$GeneSymbol %in% gene)
+    
+    melted.gep <- reshape2::melt(this_proj[[7]][index,], value.name = "GEP")
+    melted.gep <- melted.gep[order(melted.gep$GeneSymbol),]
 
     fracs <- this_proj[[1]][,-1]
     fracs <- colMeans(as.matrix(fracs))
+    
     
     #cleaner plot names
     proj_name <- strsplit(i,split = "_")[[1]]
     proj_name <- paste0(proj_name[2],"_",proj_name[3])
     
-    temp <- data.frame(cell_type = melted.gep$Group.1,
-                       GEPxFRAC = log(1 + fracs[1:7]*melted.gep$x),
-                       source = proj_name)
+    temp <- data.frame(cell_type = melted.gep$variable,
+                       GEPxFRAC = log(1 + fracs[1:7]*melted.gep$GEP),
+                       source = proj_name,
+                       SYMBOL = melted.gep$GeneSymbol)
     rownames(temp) <- NULL
     if (is.null(BigData)){
       BigData = temp
@@ -38,14 +40,10 @@ plot_GEPxFrac <- function(data = parsed_projects){
   summary(BigData)
   
   g <- ggplot(data = BigData, aes(x = source, y = GEPxFRAC, color = source)) + 
-    geom_point() + facet_wrap(facets = vars(cell_type),
-                              ncol = 4, 
+    geom_point() + facet_grid(rows = vars(SYMBOL),
+                              cols = vars(cell_type),
                               scales = "free_y") + scale_y_log10()
   g <- g + theme(axis.text.x = element_text(angle = 45, hjust = 1))
   print(g)
-  
-  g <- ggplot(data = subset(BigData,!(is.na(fracs))), aes(x = source, y = fracs, color = source)) + 
-    geom_point() + facet_wrap(facets = vars(variable))
-  g <- g + theme(axis.text.x = element_text(angle = 45, hjust = 1))
-  print(g)
+
 }
